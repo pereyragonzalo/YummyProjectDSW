c5f1cb6f9154e6ded65a935ac77f1bcfbf008a43
alejandrolbartra <alejandrolbartra@outlook.com>
2025-07-29 10:55:25 -0300

    rapido bosquejo

---
+++ Controllers/ManGeneralController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace yummyApp.Controllers
+{
+    public class ManGeneralController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace yummyApp.Models
+{
+    public class Producto
+    {
+        public int IdPro { get; set; }
+        public int NomPro { get; set; }
+        public int PrePro { get; set; }
+        public int StoPro { get; set; }
+    }
+}
