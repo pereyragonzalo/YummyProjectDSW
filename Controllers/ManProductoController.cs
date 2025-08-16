4e2be3909870b739b5c979339159cd9dab1f2eac
GonzaloPereyra <gonzalopereyra617@gmail.com>
2025-08-16 13:09:25 -0500

    cambios de bd final en la carpeta database y union del trabajo de alejandro y gonzalo

--- Controllers/ManProductoController.cs
+++ Controllers/ManProductoController.cs
@@ -2,13 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Data;
 using yummyApp.Models;
-
-
-using Microsoft.AspNetCore.Mvc;
-using yummyApp.Models;
-using System.Data;
 using Microsoft.Data.SqlClient;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace yummyApp.Controllers
 {
@@ -25,7 +19,7 @@ namespace yummyApp.Controllers
         IEnumerable<Producto> listProductos()
         {
             List<Producto> temporal = new List<Producto>();
-            using (SqlConnection cn = new SqlConnection(_config["ConnectionStrings:sql"]))
+            using (SqlConnection cn = new SqlConnection(_config["ConnectionStrings:DefaultConnection"]))
             {
                 cn.Open();
                 SqlCommand cmd = new SqlCommand("exec usp_producto", cn);
@@ -58,7 +52,7 @@ namespace yummyApp.Controllers
         //IEnumerable<Producto> paises()
         //{
         //    List<Producto> temporal = new List<Producto>();
-        //    using (SqlConnection cn = new SqlConnection(_config["ConnectionStrings:sql"]))
+        //    using (SqlConnection cn = new SqlConnection(_config["ConnectionStrings:DefaultConnection"]))
         //    {
         //        cn.Open();
         //        SqlCommand cmd = new SqlCommand("exec usp_paises", cn);
@@ -91,13 +85,13 @@ namespace yummyApp.Controllers
         {
             string mensaje = "";
 
-            using (SqlConnection cn = new SqlConnection(_config["ConnectionStrings:sql"]))
+            using (SqlConnection cn = new SqlConnection(_config["ConnectionStrings:DefaultConnection"]))
             {
                 try
                 {
                     SqlCommand cmd = new SqlCommand("usp_merge_producto", cn);
                     cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@id_producto", reg.id_producto);
+                    //cmd.Parameters.AddWithValue("@id_producto", reg.id_producto);
                     cmd.Parameters.AddWithValue("@nom", reg.nombre);
                     cmd.Parameters.AddWithValue("@prec", reg.precio);
                     cmd.Parameters.AddWithValue("@stock", reg.stock);
@@ -182,7 +176,7 @@ namespace yummyApp.Controllers
 
         public async Task<IActionResult> Delete(int id)
         {
-            using (SqlConnection cn = new SqlConnection(_config["ConnectionStrings:sql"]))
+            using (SqlConnection cn = new SqlConnection(_config["ConnectionStrings:DefaultConnection"]))
             {
                 SqlCommand cmd = new SqlCommand("usp_desactivar_producto", cn);
                 cmd.CommandType = CommandType.StoredProcedure;
